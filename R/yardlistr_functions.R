af245114e2bceb4e8177540917718536d91d1b72
Gleb Ebert <5892266+gl-eb@users.noreply.github.com>
2022-09-03 13:11:03 +0200

    further package setup und namespace fixes

--- R/yardlistr_functions.R
+++ R/yardlistr_functions.R
@@ -1,40 +1,37 @@
 # functions ---------------------------------------------------------------
 
 # return list of unique values without changing their type
-unique_values <- function(x) x |> unique() |> deframe() |> sort()
+unique_values <- function(x) x |> unique() |> tibble::deframe() |> sort()
 
 # extract time from lubridate datetime
-get_time <- function(time = now()) {
+get_time <- function(time = lubridate::now()) {
   time %>%
-    str_split(" ") %>%
-    map_chr(2) %>%
-    hms()
+    stringr::str_split(" ") %>%
+    purrr::map_chr(2) %>%
+    lubridate::hms()
 }
 
 # calculate the tetrad (one of four parts of a month) from a datetime
 tetrad <- function(x, method = "ebird") {
-  # coerce input to vector to make sure downstream operations don't fail
-  # x <- as.vector(x)
-
   # calculate tetrad according to chosen method
   if (method == "fourths") {
     # the "fourths" method divides a month into four tetrads that are as close
     # to equal in length as possible. A month with 30 days will consists of
     # tetrads of length 7, 8, 7 and 8 in this order. A month with 28 days will
     # consist of tetrads of length 7
-    tetrads <- ceiling(day(x) / (days_in_month(x) / 4))
+    tetrads <- ceiling(lubridate::day(x) / (lubridate::days_in_month(x) / 4))
   } else {
     # the "ebird" method takes day 1-7 as tetrad 1, 8-14 as tetrad 2, 15-21 as
     # tetrad 3 and >21 as tetrad 4. This method is employed by the Cornell Lab
     # of Ornithology in their bar charts on the eBird platform
-    tetrads <- ceiling(day(x) / 7) %>% replace(. == 5, 4)
+    tetrads <- ceiling(lubridate::day(x) / 7) %>% replace(. == 5, 4)
   }
 }
 
 clean_ebird_data <- function(data_file, yard_location) {
   # import latest ebird data
   raw_dat <- data_file |>
-    read_csv(show_col_types = FALSE) |>
+    readr::read_csv(show_col_types = FALSE) |>
     suppressWarnings()
 
   # filter data by location and sort by datetime
@@ -47,21 +44,21 @@ clean_ebird_data <- function(data_file, yard_location) {
     ) |>
     filter(
       Location == yard_location & # only selected location
-        !str_detect(scientific, "sp.") & # filter out spuhs
-        !str_detect(scientific, "/") # filter out slashes
+        !stringr::str_detect(scientific, "sp.") & # filter out spuhs
+        !stringr::str_detect(scientific, "/") # filter out slashes
     ) |>
-    drop_na(any_of(c("Date", "Time"))) |>
-    unite("datetime", Date:Time, sep = " ") |>
+    tidyr::drop_na(any_of(c("Date", "Time"))) |>
+    tidyr::unite("datetime", Date:Time, sep = " ") |>
     arrange(datetime) |>
     select(c("common", "scientific", "taxon", "datetime", "complete")) |>
     mutate(
-      datetime = datetime |> ymd_hms(),
-      # week = isoweek(datetime),
+      datetime = datetime |> lubridate::ymd_hms(),
+      # week = lubridate::isoweek(datetime),
       tetrad = tetrad(datetime),
-      month = month(datetime),
-      scientific = str_replace(scientific, "^", "("),
-      scientific = str_replace(scientific, "$", ")")
+      month = lubridate::month(datetime),
+      scientific = stringr::str_replace(scientific, "^", "("),
+      scientific = stringr::str_replace(scientific, "$", ")")
     ) |>
-    unite("species", c("common", "scientific"), sep = " ") |>
-    unite("month_tetrad", c("month", "tetrad"), remove = FALSE)
+    tidyr::unite("species", c("common", "scientific"), sep = " ") |>
+    tidyr::unite("month_tetrad", c("month", "tetrad"), remove = FALSE)
 }
--- R/yardlistr_main.R
+++ R/yardlistr_main.R
@@ -1,12 +1,27 @@
+#' Main yardlistr functions that calls all other helper functions
+#'
+#' @param location (character) Name of location as it appears in eBird data
+#' @param dir_dat (character) Relative or absolute path to directory containing
+#' eBird data
+#' @param dir_img (character) Relative or absolute path to directory to which
+#' plots should be saved
+#'
+#' @return A list containing the tibbles used to generate plots
+#' @export
+#'
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom fs path
+#'
 yardlistr <- function(location, dir_dat, dir_img) {
   # file system -------------------------------------------------------------
 
   # set data directories
-  dir_dat <- dir_dat |> as_fs_path()
-  dir_img <- dir_img |> as_fs_path()
+  dir_dat <- dir_dat |> fs::as_fs_path()
+  dir_img <- dir_img |> fs::as_fs_path()
 
   # get all ebird files
-  files <- dir_ls(dir_dat)
+  files <- fs::dir_ls(dir_dat)
 
 
   # basic data wrangling ----------------------------------------------------
@@ -23,7 +38,7 @@ yardlistr <- function(location, dir_dat, dir_img) {
   # tibble to store count by timepoint
   yardcount <- tibble(
     observation = seq_len(dim(checklists)[1]),
-    datetime = floor_date(checklists$datetime, unit = "day"),
+    datetime = lubridate::floor_date(checklists$datetime, unit = "day"),
     species_num = numeric(dim(checklists)[1]),
     complete = checklists$complete
   )
@@ -32,7 +47,7 @@ yardlistr <- function(location, dir_dat, dir_img) {
   # yardlist ----------------------------------------------------------------
 
   # tibble to store observed species
-  yardlist <- tibble(
+  yardlist <- tibble::tibble(
     species = character(),
     taxon = numeric()
   )
@@ -41,7 +56,7 @@ yardlistr <- function(location, dir_dat, dir_img) {
   for (i in seq_len(dim(checklists)[1])) {
     # get list of species' at current timepoint
     current_species <- dat |>
-      filter(datetime == deframe(checklists[i, "datetime"])) |>
+      filter(datetime == tibble::deframe(checklists[i, "datetime"])) |>
       select(c("species", "taxon")) |>
       unique()
     # check if any of them have not been seen before
@@ -64,14 +79,13 @@ yardlistr <- function(location, dir_dat, dir_img) {
   # heatmap -----------------------------------------------------------------
 
   # list of time units
-  # timeunits <- tibble(week = seq_len(53), n = rep(NA, 53))
   n_timeunits <- 48
-  timeunits <- tibble(
+  timeunits <- tibble::tibble(
       month = rep(seq_len(12), each = 4),
       tetrad = rep(seq_len(4), 12),
       n = rep(NA, n_timeunits)
     ) |>
-    unite("month_tetrad", c("month", "tetrad"), remove = FALSE)
+    tidyr::unite("month_tetrad", c("month", "tetrad"), remove = FALSE)
 
   # count checklists for each timeunit
   checklists_per_timeunit <- dat |>
@@ -80,11 +94,6 @@ yardlistr <- function(location, dir_dat, dir_img) {
     unique() |>
     count() |>
     ungroup()
-  # stats_checklists <- checklists_per_timeunit |>
-  #   summarise(
-  #     total = sum(n),
-  #     max = max(n)
-  #   )
   checklists_per_timeunit <- checklists_per_timeunit |>
     full_join(
       timeunits |> anti_join(checklists_per_timeunit, by = "month_tetrad"),
@@ -93,8 +102,8 @@ yardlistr <- function(location, dir_dat, dir_img) {
     mutate(frequency = n / max(n, na.rm = TRUE)) |>
     arrange(month_tetrad) |>
     select(c(month_tetrad, frequency)) |>
-    pivot_wider(names_from = month_tetrad, values_from = frequency) |>
-    add_column(
+    tidyr::pivot_wider(names_from = month_tetrad, values_from = frequency) |>
+    tibble::add_column(
       species = c("Checklists"),
       taxon = c(0)
     ) |>
@@ -114,7 +123,7 @@ yardlistr <- function(location, dir_dat, dir_img) {
   for (i in seq_along(timeunits$month_tetrad)) {
     # filter out current timeunits's data
     dat_timeunit <- dat |>
-      filter(month_tetrad == deframe(timeunits[i, "month_tetrad"]))
+      filter(month_tetrad == tibble::deframe(timeunits[i, "month_tetrad"]))
 
     # count number of checklists during timeunit
     n_checklists <- dat_timeunit |>
@@ -140,25 +149,25 @@ yardlistr <- function(location, dir_dat, dir_img) {
     # convert NAs to 0 if at least one checklist present in current tetrad
     if (n_checklists > 0) {
       dat_timeunit <- dat_timeunit |>
-        mutate(frequency = replace_na(frequency, 0))
+        mutate(frequency = tidyr::replace_na(frequency, 0))
     }
 
     frequency_per_timeunit[, i + length(columns_kept)] <- dat_timeunit |>
       select(frequency) |>
-      deframe()
+      tibble::deframe()
   }
 
   # merge two tibbles
   frequency_per_timeunit <- checklists_per_timeunit |>
     rbind(frequency_per_timeunit) |>
-    pivot_longer(
+    tidyr::pivot_longer(
       cols = (length(columns_kept) + 1):(n_timeunits + length(columns_kept)),
       names_to = "month_tetrad",
       values_to = "frequency"
     ) |>
     arrange(taxon) |>
     mutate(
-      species = species |> as_factor() |> fct_rev(),
+      species = species |> forcats::as_factor() |> forcats::fct_rev(),
       frequency = as.numeric(frequency)
     ) |>
     left_join(timeunits, by = c("month_tetrad")) |>
@@ -187,11 +196,14 @@ yardlistr <- function(location, dir_dat, dir_img) {
     filter(complete == 1) |>
     count(species, taxon) |>
     full_join(yardlist, by = c("species", "taxon")) |>
-    replace_na(list(n = 0)) |>
+    tidyr::replace_na(list(n = 0)) |>
     arrange(desc(n), taxon) %>%
     mutate(
-      species = species |> as_factor() |> fct_rev(),
-      frequency = n / complete |> select(datetime) |> deframe() |> length(),
+      species = species |> forcats::as_factor() |> forcats::fct_rev(),
+      frequency = n / complete |>
+        select(datetime) |>
+        tibble::deframe() |>
+        length(),
       frequency
     )
 
@@ -239,7 +251,8 @@ yardlistr <- function(location, dir_dat, dir_img) {
   plot_height <- max(10, round(dim(yardlist)[1] / 10) * 5)
 
   # short location name for file names
-  location_short <- str_split(location, boundary("word"))[[1]][1]
+  location_short <-
+    stringr::str_split(location, stringr::boundary("word"))[[1]][1]
 
 
   # plotting ----------------------------------------------------------------
@@ -296,8 +309,8 @@ yardlistr <- function(location, dir_dat, dir_img) {
     ggplot(aes(x = time)) +
     geom_histogram(bins = 24, fill = clr, color = "grey13") +
     scale_x_time(
-      limits = c(hours(0), hours(23)),
-      labels = label_time(format = "%H:%M")
+      limits = c(lubridate::hours(0), lubridate::hours(23)),
+      labels = scales::label_time(format = "%H:%M")
     ) +
     plot_theme +
     theme(
@@ -320,7 +333,10 @@ yardlistr <- function(location, dir_dat, dir_img) {
 
   # plot heatmap
   plot_heatmap <- frequency_per_timeunit |>
-    mutate(month_label = month(month, label = TRUE) |> as_factor()) |>
+    mutate(
+      month_label = lubridate::month(month, label = TRUE) |>
+        forcats::as_factor()
+    ) |>
     ggplot(aes(x = tetrad, y = species, fill = frequency)) +
     facet_grid(cols = vars(month_label), scales = "free_x", switch = "x") +
     geom_raster() +
@@ -390,8 +406,6 @@ yardlistr <- function(location, dir_dat, dir_img) {
       plot.title = element_text(hjust = 1),
       plot.subtitle = element_text(hjust = 1),
       axis.title = element_blank()
-      # axis.text.x = element_blank(),
-      # axis.ticks = element_blank()
     )
   # plot_frequency
   ggsave(
